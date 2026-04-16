e07ad0bb4679b608ac4cd46a7297171d7c750584
RHD-45 <111555584+RHD-45@users.noreply.github.com>
2026-04-16 23:56:25 +0700

    Bomb mechanism added

--- Assets/Script/PlayerClickToMove.cs
+++ Assets/Script/PlayerClickToMove.cs
@@ -19,15 +19,14 @@ public class PlayerClickToMove : MonoBehaviour
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             targetPosition.z = transform.position.z;
-            Debug.Log("here");
         }
         mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
     }
     void FixedUpdate()
     {
-        Vector2 direction = mousePos - rb.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+        // Vector2 direction = mousePos - rb.position;
+        // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        // rb.rotation = angle;
         Vector2 newPosition = Vector2.MoveTowards(rb.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
         rb.MovePosition(newPosition);
     }
